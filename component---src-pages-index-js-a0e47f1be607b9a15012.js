6f0ff9a3633deab9749c3e4ec6130ffb24854aeb
Oleg Baulo <oleg.baulo@inmeta.no>
2020-10-08 12:51:35 +0200

    Updates

---
+++ component---src-pages-index-js-a0e47f1be607b9a15012.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return u}));var o=t("q1tI"),r=t.n(o);function u(){return r.a.createElement("div",null,"Hello world! V2")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-a0e47f1be607b9a15012.js.map
